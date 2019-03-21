ccc3525bbdf29afe8a5ae497dd005513950fa658
wangmj1127 <46530319+wangmj1127@users.noreply.github.com>
2019-03-21 22:12:13 +0000

    Add files via upload

--- apps/accounts/tests.py
+++ apps/accounts/tests.py
@@ -1,7 +1,6 @@
 from django.test import TestCase, Client, RequestFactory
 from django.urls import reverse
-from django.utils import timezone
-import datetime
+
 
 from .models import UserAccount
 
@@ -10,10 +9,19 @@ from .models import UserAccount
 
 class AccountTest(TestCase):
 
+    # set up
     def setUp(self):
+        UserAccount.objects.create(username="testuser", password="test123", email="test@test.com")
         self.client = Client()
         self.factory = RequestFactory()
 
+    # Test if UserAccount model is created correctly
+    def test_useraccount_model(self):
+        result = UserAccount.objects.get(username="testuser")
+        self.assertEqual(result.password, "test123")
+        self.assertEqual(result.email, "test@test.com")
+
+    # validate the registration
     def test_validate_register(self):
         self.assertEquals(0, len(UserAccount.objects.filter(email='user123@user.com')))
         response = self.client.post(reverse('account:register'), {
@@ -24,5 +32,22 @@ class AccountTest(TestCase):
         })
 
         self.assertEquals(1, len(UserAccount.objects.filter(email='user123@user.com')))
-
+        # Test if user1233 can login in successfully
         self.client.login(username='user1233', password='password123!q@wE#R$T')
+        # Test if user1233 can access in to account page after login
+        response = self.client.get('/account/')
+        self.assertEqual(response.status_code, 200)
+
+    # Test the response of the login page
+    def test_login_page(self):
+        response = self.client.get('/login/')
+        self.assertEqual(response.status_code, 200)
+
+    # Test the response of the register page
+    def test_register_page(self):
+        response = self.client.get('/register/')
+        self.assertEqual(response.status_code, 200)
+
+
+
+
